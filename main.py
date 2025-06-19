f6ef7e9795ab75fc8c9acf0272cd753f74d194ea
Joshua <jronley@btinternet.com>
2025-06-19 18:12:19 +0100

    testing CI/CD pipeline

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"This is": "another test"}
-
-
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
