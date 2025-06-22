90f58ebde7ae1ad15a2a8f1ebde2751229b15ae8
Joshua <jronley@btinternet.com>
2025-06-22 12:22:12 +0100

    changed origins

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 origins = [
     "https://victorious-bay-07769b703.1.azurestaticapps.net",
-    "http://localhost:3000",  
+    "http://localhost:5173",  
 ]
 
 app.add_middleware(
