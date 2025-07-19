083ba46497473c4b93b7b4eb88a3eb96d5853f16
Joshua <jronley@btinternet.com>
2025-07-19 22:10:44 +0100

    Trigger redeploy. Refactored backend has broken deployment. Attempting to fix

--- backend/database.py
+++ backend/database.py
@@ -3,16 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from config import get_settings
 
-load_dotenv()
+settings = get_settings()
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://username:password@localhost/dbname"
-)
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
